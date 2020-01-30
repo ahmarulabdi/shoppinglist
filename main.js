9a95f991800108d868579964a4f3a404f24f8db2
Ahmarul Abdi <abdi@dropshipkit.com>
2020-01-30 16:45:19 +0700

    adding html file and main js for electron

---
+++ main.js
@@ -0,0 +1,46 @@
+const electron = require('electron')
+const url = require('url')
+const path = require('path')
+
+const { app, BrowserWindow, Menu} = require('electron')
+
+let mainWIndow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+    // Create new Window
+    mainWIndow = new BrowserWindow({});
+
+    // Load html into wndow
+    mainWIndow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    // Build Menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+    Menu.setApplicationMenu(mainMenu)
+});
+
+// Create menu template
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add Item'
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit',
+                click() {
+                    app.quit()
+                }
+            }
+        ]
+    }
+]
