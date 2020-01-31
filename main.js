9fd024a3eb41f98b51719772fc8ebdf7d47260e6
Ahmarul Abdi <abdi@dropshipkit.com>
2020-01-31 10:29:05 +0700

    formating code

--- main.js
+++ main.js
@@ -1,65 +1,69 @@
-const electron = require('electron')
-const url = require('url')
-const path = require('path')
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
 
-const { app, BrowserWindow, Menu} = require('electron')
+const { app, BrowserWindow, Menu } = require("electron");
 
-let mainWIndow
-let addWindow
+let mainWIndow;
+let addWindow;
 
 // Listen for app to be ready
-app.on('ready', function(){
-    // Create new Window
-    mainWIndow = new BrowserWindow({});
+app.on("ready", function() {
+  // Create new Window
+  mainWIndow = new BrowserWindow({});
 
-    // Load html into wndow
-    mainWIndow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+  // Load html into wndow
+  mainWIndow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "mainWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
 
-    // Build Menu from template
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
-    Menu.setApplicationMenu(mainMenu)
+  // Build Menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
 
-createAddWindow() {
-    addWindow = new BrowserWindow({
-        width: 300,
-        height: 200,
-        title: "Add Shopping List Item"
-    })
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add Shopping List Item"
+  });
 
-    addWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'addWindow.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
 }
 
 // Create menu template
 
 const mainMenuTemplate = [
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Add Item',
-                 click() {
-                     createAddWindow()
-                 }
-            },
-            {
-                label: 'Clear Items'
-            }, 
-            {
-                label: 'Quit',
-                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-                click() {
-                    app.quit()
-                }
-            }
-        ]
-    }
-]
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: "Clear Items"
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
